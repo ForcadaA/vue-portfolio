4bfac58430043b9172fcffa1f4a364496d731c9b
ForcadaA <87941379+ForcadaA@users.noreply.github.com>
2022-05-16 10:04:31 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from '../vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from '@/router'
+import router from './src/router'
 
 console.log(cwd)
 
