634742476ec1b9db0cc4b597f0ec43423f621ae4
ForcadaA <forcada.adrien@gmail.com>
2022-05-16 13:52:55 +0200

    image src in profile updated

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -3,13 +3,13 @@
         <input type="checkbox" id="nav-toggle" class="nav-toggle">
         <label for="nav-toggle"><img class="nav-logo" src="../assets/favicon.png" alt="logo"></label>
         <div class="nav-link-container">
-            <router-link to="/">
+            <router-link to="/vue-portfolio/">
             Home
             </router-link>
             <router-link
             v-for="field in profile"
             :key="field.id"
-            :to="`/profile/${field.id}`"
+            :to="`/vue-portfolio/profile/${field.id}`"
             >
                 {{field.title}}
             </router-link>
